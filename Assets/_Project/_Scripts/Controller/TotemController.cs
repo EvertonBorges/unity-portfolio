501f19f9cbba65f8f5300eb2212b4d63793ca13a
Everton Borges <evertonsoaresborges@gmail.com>
2024-03-04 12:14:12 -0400

    fix: Fix problem to load videos on WebGL build

--- Assets/_Project/_Scripts/Controller/TotemController.cs
+++ Assets/_Project/_Scripts/Controller/TotemController.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -7,6 +8,7 @@ public class TotemController : MonoBehaviour
 
     [Header("Video Parameters")]
     [SerializeField] private VideoPlayer _videoPlayer;
+    [SerializeField] private string _videoName;
 
     [Header("Events")]
     [SerializeField] private InspectorEvent _activeEvent;
@@ -15,6 +17,9 @@ public class TotemController : MonoBehaviour
     void Awake()
     {
         _videoPlayer ??= GetComponent<VideoPlayer>();
+
+        var path = Path.Combine(Application.dataPath, "Resources", "Videos", _videoName);
+        _videoPlayer.url = path;
 
         Manager_Events.Add(_activeEvent, ActiveEvent);
         Manager_Events.Add(_desactiveEvent, DesactiveEvent);
